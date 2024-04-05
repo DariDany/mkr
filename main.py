ec466fc2e62a1f97d12f7d193369e6875d2065aa
DariDany <daridanyi70405@gmail.com>
2024-04-05 11:27:14 +0300

    Added the function to find the most popular words

---
+++ main.py
@@ -0,0 +1,11 @@
+from collections import Counter
+import re
+
+def find_popular_words(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+        words = re.findall(r'\b\w+\b', text.lower())
+        word_counts = Counter(words)
+        top_words = word_counts.most_common(10)
+        return top_words
+
